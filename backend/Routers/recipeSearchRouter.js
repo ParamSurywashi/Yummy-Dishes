b3a0df4f5ce13356fea2421ec304020c20dd2b6f
ParamSurywashi <parmanandchared1997@gmail.com>
2023-04-10 12:31:17 +0530

    updated

--- backend/Routers/recipeSearchRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const recipeController = require('../Controller/recipeController');
-const router = express.Router();
-
-
-router.get('/:data',recipeController.searchRecipe);
-
-exports.recipeSearchRoutes=router;
